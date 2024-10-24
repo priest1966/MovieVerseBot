d84c74e6e7ee4ab58991c933cd2f73154ec37886
priest1966 <53721478+priest1966@users.noreply.github.com>
2024-10-24 19:08:48 +0530

    update

--- plugins/pmfilter.py
+++ plugins/pmfilter.py
@@ -3427,7 +3427,8 @@ async def auto_filter(client, msg, spoll=False):
     
     if not spoll:
         message = msg
-        if message.text.startswith("/"): return  # ignore commands
+        if message.text and message.text.startswith("/"):
+    return  # ignore commands
         if re.findall("((^\/|^,|^!|^\.|^[\U0001F600-\U000E007F]).*)", message.text):
             return
         if len(message.text) < 100:
