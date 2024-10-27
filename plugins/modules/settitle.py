a20b4ca0684fa691c34d0fe954e7c3fdfa3a4a7d
priest1966 <53721478+priest1966@users.noreply.github.com>
2024-10-27 13:30:10 +0530

    update

--- plugins/modules/settitle.py
+++ plugins/modules/settitle.py
@@ -46,10 +46,6 @@ PROTECT_CONTENT = True  # Set this flag based on your needs
 from pyrogram import Client, filters
 from Script import GREETINGS_TEXT  # Import GREETINGS_TEXT from script.py
 
-@Client.on_message(filters.group & filters.command("greetings"))
-def greetings_command(client, message):
-    # Respond with the greetings text from script.py
-    message.reply_text(GREETINGS_TEXT, disable_web_page_preview=True)
 
 @Client.on_message(filters.command("rules") & filters.group)
 async def r_message(client, message):
